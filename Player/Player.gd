0d85dec56b55ce4d379b1ae94d7c2e0c0db57016
KalabRobertson <97589909+KalabRobertson@users.noreply.github.com>
2022-02-12 23:01:09 -0500

    Complete Project

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,11 +5,14 @@ var speed = 5.0
 var max_speed = 400.0
 var rot_speed = 5.0
 
-var nose = Vector2(0,-70)
+var nose = Vector2(0,-60)
 
-var health = 100.0
+
+var health = 50.0
 
 onready var Bullet = load("res://Player/Bullet.tscn")
+onready var Explosion = load("res://Effects/Explosion.tscn")
+
 var Effects = null
 
 
@@ -51,6 +54,12 @@ func shoot():
 func damage(d):
 	health -= d
 	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instance()
+			explosion.global_position = global_position
+			Effects.add_child(explosion)
+		Global.update_lives(-1)
 		queue_free()
 
 
